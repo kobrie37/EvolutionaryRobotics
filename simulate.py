53cdb2896e4cce38d9c84e8f3343d1c29d98ec6d
Kate O'Brien <kate.obrien@uvm,edu>
2025-01-28 20:45:38 -0500

    Joints

--- simulate.py
+++ simulate.py
@@ -1,15 +1,22 @@
 import pybullet as p 
 import time 
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 step = 1/45
 
-p.loadSDF("box.sdf")
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+
+p.loadSDF("world.sdf")
+
+robotId = p.loadURDF("body.urdf")
 
 for i in range(0, 1000, 1):
-    p.stepSimulation
+    p.stepSimulation()
     time.sleep(step)
     print(i)
 p.disconnect() 
 
-p.configureDegugVisualizer(p.COV_ENABLE_GUI,0) 
