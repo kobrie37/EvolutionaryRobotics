b1f4e3c8093a2411e288a58f5931a634481f1db1
Kate O'Brien <kate.obrien@uvm,edu>
2025-02-05 12:42:02 -0500

    Sensor updates to simulate and generate before analyze

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p 
 import time 
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy as np 
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -14,9 +16,16 @@ p.loadSDF("world.sdf")
 
 robotId = p.loadURDF("body.urdf")
 
+pyrosim.Prepare_To_Simulate(robotId)
+
+frontLegSensorValues = np.zeros(1000)
+
 for i in range(0, 1000, 1):
     p.stepSimulation()
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(step)
-    print(i)
+    #print(i)
 p.disconnect() 
 
+print(frontLegSensorValues)
+np.save("FrontLegSensorValues.npy", frontLegSensorValues)
