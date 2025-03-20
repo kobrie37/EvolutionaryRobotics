c55e504962715f8bc9f47210637dba7d8603a94d
Kate O'Brien <kate.obrien@uvm,edu>
2025-03-20 19:21:05 -0400

    createdhillclimber

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 import os as os
+from hillclimber import HILL_CLIMBER 
 
 for i in range(2):
-    os.system("python3 generate.py")
-    os.system("python3 simulate.py")
+    #os.system("python3 generate.py")
+    #os.system("python3 simulate.py")
